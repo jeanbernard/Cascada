960063af5099cebfbe1f78786d34205c2dff4c20
Jean Bernard <jeanmarinobernard@gmail.com>
2015-11-17 20:47:00 -0400

    Updated Spring version.

--- src/main/java/com/cascada/controller/HomeController.java
+++ src/main/java/com/cascada/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.cascada.nomina;
+package com.cascada.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
---
+++ src/main/java/com/cascada/domain/Departamento.java
@@ -0,0 +1,44 @@
+package com.cascada.domain;
+
+/**
+ * Created by jeanbernard on 11/17/15.
+ */
+public class Departamento {
+
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private int estado;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+}
