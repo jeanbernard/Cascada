53e8631ff17487c44425b8283986159b35f98031
Jean Bernard <jeanmarinobernard@gmail.com>
2015-11-20 00:59:13 -0400

    JavaScript

---
+++ src/main/webapp/resources/js/cascada.js
@@ -0,0 +1,14 @@
+/**
+ * Created by jeanbernard on 11/20/15.
+ */
+
+$(document).ready(function(){
+
+
+});
+
+
+$("li").click(function() {
+    $(this).removeClass("active");
+    $(this).addClass("active");
+});
